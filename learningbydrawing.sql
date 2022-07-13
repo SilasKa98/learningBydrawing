6254031e0efafb919522f33c2a028994aa1eb39a
SilasKa98 <silas.kammerer@googlemail.com>
2022-07-13 20:23:58 +0200

    changed the db schema and added a table which contains all datasets

--- learningbydrawing.sql
+++ learningbydrawing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 12. Jul 2022 um 18:17
+-- Erstellungszeit: 13. Jul 2022 um 20:23
 -- Server-Version: 10.4.24-MariaDB
 -- PHP-Version: 7.4.28
 
@@ -20,6 +20,25 @@ SET time_zone = "+00:00";
 --
 -- Datenbank: `learningbydrawing`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `datasets`
+--
+
+CREATE TABLE `datasets` (
+  `ID` int(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `data` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `datasets`
+--
+
+INSERT INTO `datasets` (`ID`, `category`, `data`) VALUES
+(1, 'Zahlen', 'Null,Eins,Zwei,Drei,Vier,Fünf,Sechs,Sieben,Acht,Neun');
 
 -- --------------------------------------------------------
 
@@ -41,9 +60,16 @@ CREATE TABLE `learningresults` (
 --
 
 INSERT INTO `learningresults` (`ID`, `uuid`, `category`, `tested_value`, `right_answers`, `wrong_answers`) VALUES
-(11, '62cd8d8180eea', 'Zahlen', '9', 0, 1),
-(12, '62cd8d8180eea', 'Zahlen', '3', 1, 0),
-(13, '62cd8d8180eea', 'Zahlen', '8', 0, 1);
+(14, '62cd8d8180eea', 'Zahlen', 'Sieben', 3, 1),
+(15, '62cd8d8180eea', 'Zahlen', 'Neun', 1, 25),
+(16, '62cd8d8180eea', 'Zahlen', 'Zwei', 1, 0),
+(17, '62cd8d8180eea', 'Zahlen', 'Vier', 4, 2),
+(18, '62cd8d8180eea', 'Zahlen', 'Sechs', 5, 13),
+(19, '62cd8d8180eea', 'Zahlen', 'Fünf', 4, 0),
+(20, '62cd8d8180eea', 'Zahlen', 'Eins', 7, 5),
+(21, '62cd8d8180eea', 'Zahlen', 'Null', 3, 1),
+(22, '62cd8d8180eea', 'Zahlen', 'Acht', 6, 10),
+(23, '62cd8d8180eea', 'Zahlen', 'Drei', 7, 10);
 
 -- --------------------------------------------------------
 
@@ -52,7 +78,6 @@ INSERT INTO `learningresults` (`ID`, `uuid`, `category`, `tested_value`, `right_
 --
 
 CREATE TABLE `loginsystem` (
-  `ID` int(255) NOT NULL,
   `user_id` varchar(255) NOT NULL,
   `user_email` varchar(255) NOT NULL,
   `user_pwd` varchar(255) NOT NULL,
@@ -63,8 +88,8 @@ CREATE TABLE `loginsystem` (
 -- Daten für Tabelle `loginsystem`
 --
 
-INSERT INTO `loginsystem` (`ID`, `user_id`, `user_email`, `user_pwd`, `uuid`) VALUES
-(3, 'Silas', 'Silas@silas.de', '$2y$10$KzHVkbjnql0m.0NOUVqJrOtwJea5hBe98bXKtW9f0rPGP1cWLg3f.', '62cd8d8180eea');
+INSERT INTO `loginsystem` (`user_id`, `user_email`, `user_pwd`, `uuid`) VALUES
+('Silas', 'Silas@silas.de', '$2y$10$KzHVkbjnql0m.0NOUVqJrOtwJea5hBe98bXKtW9f0rPGP1cWLg3f.', '62cd8d8180eea');
 
 -- --------------------------------------------------------
 
@@ -83,16 +108,23 @@ CREATE TABLE `settings` (
 --
 
 --
+-- Indizes für die Tabelle `datasets`
+--
+ALTER TABLE `datasets`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indizes für die Tabelle `learningresults`
 --
 ALTER TABLE `learningresults`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `fk_uuid` (`uuid`);
 
 --
 -- Indizes für die Tabelle `loginsystem`
 --
 ALTER TABLE `loginsystem`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`uuid`);
 
 --
 -- Indizes für die Tabelle `settings`
@@ -105,22 +137,32 @@ ALTER TABLE `settings`
 --
 
 --
+-- AUTO_INCREMENT für Tabelle `datasets`
+--
+ALTER TABLE `datasets`
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT für Tabelle `learningresults`
 --
 ALTER TABLE `learningresults`
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
---
--- AUTO_INCREMENT für Tabelle `loginsystem`
---
-ALTER TABLE `loginsystem`
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT für Tabelle `settings`
 --
 ALTER TABLE `settings`
   MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `learningresults`
+--
+ALTER TABLE `learningresults`
+  ADD CONSTRAINT `fk_uuid` FOREIGN KEY (`uuid`) REFERENCES `loginsystem` (`uuid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
