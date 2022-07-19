70c12b931cf39c545a82eafb3a9424bb530af3d2
SilasKa98 <silas.kammerer@googlemail.com>
2022-07-19 21:30:13 +0200

    removed comments

--- python/learningKuzushiji.py
+++ python/learningKuzushiji.py
@@ -10,7 +10,6 @@ from sklearn.model_selection import train_test_split
 batch_size = 64
 SEED = 444
 
-# se carga la data de entrenamiento y prueba
 
 os.listdir('learningData/kuzushiji')
 
@@ -26,15 +25,15 @@ print(train_labels)
 train_labels = keras.utils.to_categorical(train_labels, num_class)
 test_labels = keras.utils.to_categorical(test_labels, num_class)
 
-# se crea el set de validación
+
 train_set, validation_set, train_labels, validation_labels = train_test_split(train_set, train_labels, test_size = 0.1, random_state = SEED)
 
-# normalización del dataset
+
 train_set = train_set.reshape(train_set.shape[0], train_set.shape[1], train_set.shape[2], 1)/255
 validation_set = validation_set.reshape(validation_set.shape[0], validation_set.shape[1], validation_set.shape[2], 1)/255
 test_set = test_set.reshape(test_set.shape[0], test_set.shape[1], test_set.shape[2], 1)/255
 
-# dimensiones del dataset
+
 print(f"""
 Train set dimensions: {train_set.shape}
 Validation set dimensions: {validation_set.shape}
@@ -45,19 +44,19 @@ Test set dimensions: {test_set.shape}
 early_stopping = EarlyStopping(monitor='val_loss', patience=5)
 
 model = tf.keras.Sequential()
-# primera capa
+
 model.add(tf.keras.layers.Conv2D(32,(5,5),activation = 'relu', input_shape = (28,28,1), padding="same"))
 model.add(tf.keras.layers.MaxPooling2D(2,2))
 model.add(tf.keras.layers.BatchNormalization())
-# segunda capa
+
 model.add(tf.keras.layers.Conv2D(64,(5,5),activation = 'relu'))
-# tercera capa
+
 model.add(tf.keras.layers.Conv2D(64,(5,5),activation = 'relu'))
 model.add(tf.keras.layers.Flatten())
-# cuarta capa
+
 model.add(tf.keras.layers.Dense(64,activation = 'relu'))
 model.add(tf.keras.layers.Dropout(0.4))
-# quinta capa
+
 model.add(tf.keras.layers.Dense(num_class,activation = 'softmax'))
 
 model.summary()
