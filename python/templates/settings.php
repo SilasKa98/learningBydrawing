fd944b2ccd42e346349999a234a5ab150b9feed5
SilasKa98 <silas.kammerer@googlemail.com>
2022-08-01 14:22:22 +0200

    updated title of settings page

--- python/templates/settings.php
+++ python/templates/settings.php
@@ -42,7 +42,7 @@ if(isset($_SESSION["idUser"])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Einstellungen</title>
+    <title>Lernauswahl</title>
     <link rel="stylesheet" href="styles/styles.css">
     <link rel="stylesheet" href="styles/background.css">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
